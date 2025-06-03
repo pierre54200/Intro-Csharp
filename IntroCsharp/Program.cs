84cdabe333d24984b6d99692bb292ce51d50329e
ILPinciatore <pierre.maciejewski@epitech.eu>
2025-06-03 08:00:40 +0200

    [ADD] trying out C# / testing basic console test

---
+++ IntroCsharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using Microsoft.Win32.SafeHandles;
+
+Console.WriteLine("Intro C#");
+
+Console.WriteLine("Entre ton nom");
+string name = Console.ReadLine();
+
+Console.WriteLine("Entre ton age");
+int age = int.Parse(Console.ReadLine());
+
+if (age < 18)
+    Console.WriteLine($"Bonjour {name} vous avez {age} ans tu es mineur");
+else
+    Console.WriteLine($"Bonjour {name} vous avez {age} ans tu es majeur");
